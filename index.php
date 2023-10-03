a2668c3c4b31d94e80d87e40c448fde3484c3cf4
Sandysan1234 <140880265+Sandysan1234@users.noreply.github.com>
2023-10-03 21:07:27 +0700

    first-commit
    upload repo pertamaku

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php require_once "view/header.php";
+?>
+
+    <section id="hero">
+      <h1>Find Dolls your dear Here!</h1>
+      <p>
+        “Doll, friend in silence. In his warm embrace, stories were told. Traces
+        of memories and laughter are etched in the threads. When alone, he
+        accompanies faithfully. Eternal friendship, in a sweet form.”
+      </p>
+      <button>Shop Now!!</button>
+    </section>
+
+    <div class="features-product">
+      <h1>Our Featured Product</h1>
+      <div class="container-card">
+        <div class="card">
+          <img src="img/features/f1.png" alt="Avatar" />
+          <div class="container">
+            <h4><b>Baby Dolls</b></h4>
+          </div>
+        </div>
+        <div class="card">
+          <img src="img/features/f1.png" alt="Avatar" />
+          <div class="container">
+            <h4><b>Baby Dolls</b></h4>
+          </div>
+        </div>
+        <div class="card">
+          <img src="img/features/f1.png" alt="Avatar" />
+          <div class="container">
+            <h4><b>Baby Dolls</b></h4>
+          </div>
+        </div>
+        <div class="card">
+          <img src="img/features/f1.png" alt="Avatar" />
+          <div class="container">
+            <h4><b>Baby Dolls</b></h4>
+          </div>
+        </div>
+
+        <div class="card">
+          <img src="img/features/f1.png" alt="Avatar" />
+          <div class="container">
+            <h4><b>Baby Dolls</b></h4>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="contact">
+      <h1>Contact</h1>
+      <h4>
+        Contact us anytime, we are happy to provide you with full support!
+      </h4>
+      <div class="map">
+        <div class="gmap_canvas">
+          <iframe
+            src="https://maps.google.com/maps?q=universitas%20muhammadiyah%20sidoarjo&amp;t=&amp;z=20&amp;ie=UTF8&amp;iwloc=&amp;output=embed"
+            frameborder="0"
+            scrolling="no"
+          ></iframe>
+        </div>
+      </div>
+
+      <div class="contact-me">
+        <div class="alamat">
+          <img src="img/ss.png" alt="S">
+          <p>Jl. Raya Gelam No.250, Pagerwaja, Gelam, Kec. Candi, Kabupaten Sidoarjo, Jawa Timur 61271</p>
+          <h5>Telepon : 085102710024</h5>
+        </div>
+        <div class="identitas">
+          <h4>Contact me</h4>
+          <input type="text" class="input name" name="myname" placeholder="Name"><br>
+          <input type="text" class="input address" name="myaddress" placeholder="Address"><br>
+          <input type="text" class="input email" name="myemail" placeholder="Email"><br>
+          <input type="text" class="input phone" name="myphone" placeholder="No Phone"><br>
+          <textarea class="input" name="pesan" rows="10
+          " required="" placeholder="Tuliskan pesan Anda"></textarea>
+          <button>Send</button>
+        </div>
+      </div>
+
+    </div>
+
+    <script src="script.js"></script>
+
+<?php require_once "view/footer.php"; ?>
+
---
+++ view/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>E commerce</title>
+    <link rel="shortcut icon" href="./img/a.jpg" type="image/x-icon" />
+    <script src="https://kit.fontawesome.com/6b3006de54.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="/bootstrap-5.0.2-dist/css/bootstrap.css" />
+    <link rel="stylesheet" href="view/style.css">
+  </head>
+  <body>
+  <section id="header">
+      <a href=""><img src="./img/llogo.png" class="logo" alt="" /></a>
+
+      <div>
+        <ul id="navbar">
+          <li><a class="active" href="index.php">Home</a></li>
+          <li><a href="shop.html">Shop</a></li>
+          <li><a href="About.html">Contact</a></li>
+          <li>
+            <a href=""><i class="fa fa-sharp fa-solid fa-cart-plus"></i></a>
+          </li>
+        </ul>
+      </div>
+    </section>
